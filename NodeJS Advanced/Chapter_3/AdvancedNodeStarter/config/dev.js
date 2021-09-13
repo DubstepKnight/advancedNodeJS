fdb3bf0e1b1a53c17f2a5692b2fd724c82f3efe3
Nursultan Akhmetzhanov <nursultan.akhmetzhanov@codemate.com>
2021-09-13 14:25:09 +0300

    FInished chapters/sections 3 and 4

---
+++ NodeJS Advanced/Chapter_3/AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://admin:bbyue8mvjrb7mWJF@advancednodejs-shard-00-00.cpsfk.mongodb.net:27017,advancednodejs-shard-00-01.cpsfk.mongodb.net:27017,advancednodejs-shard-00-02.cpsfk.mongodb.net:27017/AdvancedNodeJS?ssl=true&replicaSet=atlas-d5l6n5-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
